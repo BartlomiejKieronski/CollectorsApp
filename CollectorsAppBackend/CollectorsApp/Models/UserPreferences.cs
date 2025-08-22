46a1e65128c6c3664e9baa780640261f6b54fedc
Bartłomiej Kieroński <84330061+BartlomiejKieronski@users.noreply.github.com>
2025-08-22 14:26:15 +0200

    Added user persistent preferences table

---
+++ CollectorsAppBackend/CollectorsApp/Models/UserPreferences.cs
@@ -0,0 +1,22 @@
+﻿using CollectorsApp.Models.Interfaces;
+using ServiceStack.DataAnnotations;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CollectorsApp.Models
+{
+    public class UserPreferences : IOwner
+    {
+        [NotNull, AutoIncrement]
+        public int Id { get; set; }
+        [NotNull]
+        public int OwnerId {  get; set; }
+        [NotNull, DefaultValue("Classical")]
+        public string Layout { get; set; }
+        public string Theme { get; set; }
+        [AllowNull, DefaultValue(10)]
+        public int ItemsPerPage { get; set; }
+        [AllowNull, DefaultValue(true)]
+        public bool Pagination { get; set; }
+    }
+}
