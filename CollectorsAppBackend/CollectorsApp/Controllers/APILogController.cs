3688dd0bf2e75d3bdb7e337728d857b3de5ce396
Bartłomiej Kieroński <84330061+BartlomiejKieronski@users.noreply.github.com>
2025-08-22 14:17:00 +0200

    Logs controllers

---
+++ CollectorsAppBackend/CollectorsApp/Controllers/APILogController.cs
@@ -0,0 +1,64 @@
+﻿using CollectorsApp.Models.Analytics;
+using CollectorsApp.Models.APILogs;
+using CollectorsApp.Models.Filters;
+using CollectorsApp.Repository.AnalyticsRepositories.AnalyticsRepositoryInterfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectorsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APILogController : ControllerBase
+    {
+        private readonly IAPILogRepository _repository;
+        public APILogController(IAPILogRepository repository) 
+        {
+            _repository = repository;
+        }
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        [Route("query")]
+        public async Task<ActionResult<IEnumerable<APILog>>> QueryComments([FromQuery] APILogFilter entity)
+        {
+            return Ok(await _repository.QueryEntity(entity));
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<APILog>>> GetAll()
+        {
+            return Ok(await _repository.GetAllAsync());
+        }
+        [Authorize(Roles = "admin")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<APILog>> GetAPILog(int id)
+        {
+            return Ok(await _repository.GetByIdAsync(id));
+        }
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public async Task<ActionResult> PostAPILog(APILog log)
+        {
+            await _repository.PostAsync(log);
+            return CreatedAtAction("Created Succesfully", log);
+        }
+        [Authorize(Roles = "admin")]
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutAPILog(int id, APILog log)
+        {
+            await _repository.UpdateAsync(log, id);
+            return NoContent();
+        }
+        [Authorize(Roles = "admin")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAPILog(int id)
+        {
+            bool isSuccesful = await _repository.DeleteAsync(id);
+            if (isSuccesful)
+                return NoContent();
+            return BadRequest();
+        }
+    }
+}
---
+++ CollectorsAppBackend/CollectorsApp/Controllers/AdminCommentsController.cs
@@ -0,0 +1,65 @@
+﻿using CollectorsApp.Models.Analytics;
+using CollectorsApp.Models.Filters;
+using CollectorsApp.Repository.AnalyticsRepositories.AnalyticsRepositoryInterfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectorsApp.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminCommentsController : Controller
+    {
+        private readonly IAdminCommentsRepository _repository;
+        
+        public AdminCommentsController(IAdminCommentsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        [Route("query")]
+        public async Task<ActionResult<IEnumerable<AdminComment>>> QueryComments([FromQuery] AdminCommentFilter entity)
+        {
+            return Ok(await _repository.QueryEntity(entity));
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AdminComment>>> GetAll()
+        {
+            return Ok(await _repository.GetAllAsync());
+        }
+        [Authorize(Roles = "admin")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AdminComment>> GetComment(int id)
+        {
+            return Ok(await _repository.GetByIdAsync(id));
+        }
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public async Task<ActionResult> PostComment(AdminComment comment)
+        {
+            await _repository.PostAsync(comment);
+            return  CreatedAtAction("Created Succesfully", comment);
+        }
+        [Authorize(Roles = "admin")]
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutComment(int id, AdminComment comment)
+        {
+            await _repository.UpdateAsync(comment,id);
+            return NoContent();
+        }
+        [Authorize(Roles = "admin")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteComment(int id)
+        {
+            bool isSuccesful = await _repository.DeleteAsync( id);
+            if(isSuccesful)
+                return NoContent();
+            return BadRequest();
+        }
+    }
+}
