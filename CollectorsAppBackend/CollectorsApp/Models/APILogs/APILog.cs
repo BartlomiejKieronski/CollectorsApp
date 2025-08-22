3bff55ce205619e798f666d7bfaf922f005adcf1
Bartłomiej Kieroński <84330061+BartlomiejKieronski@users.noreply.github.com>
2025-08-22 14:16:32 +0200

    Logs models

---
+++ CollectorsAppBackend/CollectorsApp/Models/APILogs/APILog.cs
@@ -0,0 +1,39 @@
+﻿using ServiceStack.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CollectorsApp.Models.APILogs
+{
+    public class APILog
+    {
+        [NotNull, AutoIncrement]
+        public int Id { get; set; }
+        [AllowNull]
+        public int? UserId { get; set; }
+        [AllowNull]
+        public string? Controller { get; set; }
+        [AllowNull]
+        public string? Action { get; set; }
+        [AllowNull]
+        public int StatusCode { get; set; }
+        [AllowNull]
+        public bool IsSuccess { get; set; }
+        [AllowNull]
+        public string? Title { get; set; }
+        [AllowNull]
+        public string? Description { get; set; }
+        [AllowNull]
+        public string? ErrorMessage { get; set; }
+        [AllowNull]
+        public string? RequestPath { get; set; }
+        [AllowNull]
+        public string? HttpMethod { get; set; }
+        [AllowNull]
+        public string? IpAddress { get; set; }
+        [AllowNull]
+        public string? IpIV { get; set; }
+        [NotNull]
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+        [AllowNull]
+        public int? DurationMs { get; set; }
+    }
+}
---
+++ CollectorsAppBackend/CollectorsApp/Models/APILogs/AdminComment.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStack.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CollectorsApp.Models.Analytics
+{
+    public class AdminComment
+    {
+        [NotNull,AutoIncrement]
+        public int Id { get; set; }
+        [NotNull]
+        public int AdminId { get; set; }
+        [NotNull]
+        public int EventLogId { get; set; }
+        [NotNull]
+        public string TargetType { get; set; }
+        [NotNull]
+        public string CommentText { get; set; }
+        [AllowNull]
+        public DateTime? TimeStamp { get; set; } = DateTime.UtcNow;
+    }
+}
