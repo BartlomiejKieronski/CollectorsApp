71e9ca0731b74a9c73e690ce406d816caef2c42b
Bartłomiej Kieroński <84330061+BartlomiejKieronski@users.noreply.github.com>
2025-08-22 14:15:43 +0200

    Filters for FromQuery requests

---
+++ CollectorsAppBackend/CollectorsApp/Models/Filters/APILogFilter.cs
@@ -0,0 +1,27 @@
+﻿namespace CollectorsApp.Models.Filters
+{
+    public class APILogFilter
+    {
+        public int? Id { get; set; }
+        public int? UserId { get; set; }
+        public string? Controller { get; set; }
+        public string? Action { get; set; }
+        public int? StatusCode { get; set; }
+        public bool? IsSuccess { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string? RequestPath { get; set; }
+        public string? HttpMethod { get; set; }
+        public string? IpAddress { get; set; }
+        public string? IpIV { get; set; }
+        public DateTime? TimeStamp { get; set; } = DateTime.UtcNow;
+        public DateTime? After { get; set; }
+        public DateTime? Before { get; set; }
+        public string? OrderBy { get; set; }
+        public string? SortBy { get; set; }
+        public int? Page { get; set; }
+        public int? NumberOfItems { get; set; }
+        public int? DurationMs { get; set; }
+    }
+}
---
+++ CollectorsAppBackend/CollectorsApp/Models/Filters/AdminCommentFilter.cs
@@ -0,0 +1,18 @@
+﻿namespace CollectorsApp.Models.Filters
+{
+    public class AdminCommentFilter
+    {
+        public int? Id { get; set; }
+        public DateTime? TimeStamp { get; set; }
+        public DateTime? Before { get; set; }
+        public DateTime? After { get; set; }
+        public string? CommentText { get; set; }
+        public int? AdminId { get; set; }
+        public int? EventLogId { get; set; }
+        public string? TargetType { get; set; }
+        public int? NumberOfRecords { get; set; }
+        public int? Page { get; set; }
+        public string? OrderBy { get; set; }
+        public bool? SortDescending { get; set; }
+    }
+}
