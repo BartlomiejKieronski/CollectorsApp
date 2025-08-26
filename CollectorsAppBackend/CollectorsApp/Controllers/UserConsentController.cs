75b28405e55f1adb00b63c054ae2f9050dc3b34a
Bartłomiej Kieroński <84330061+BartlomiejKieronski@users.noreply.github.com>
2025-08-26 10:08:24 +0200

    Added controllers for user consent and preferences

---
+++ CollectorsAppBackend/CollectorsApp/Controllers/UserConsentController.cs
@@ -0,0 +1,56 @@
+﻿using CollectorsApp.Models;
+using CollectorsApp.Repository.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectorsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserConsentController : ControllerBase
+    {
+        private readonly IUserConsentRepository _repository;
+        public UserConsentController(IUserConsentRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public async Task<ActionResult<IEnumerable<UserConsent>>> GetAll()
+        {
+            return Ok(await _repository.GetAllAsync());
+        }
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<ActionResult<UserConsent>> GetById(int id)
+        {
+            return Ok(await _repository.GetByIdAsync(id));
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post(UserConsent entity)
+        {
+            await _repository.PostAsync(entity);
+            return CreatedAtAction("Created Succesfully", entity);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, UserConsent entity)
+        {
+            if (id != entity.Id)
+            {
+                return BadRequest(new { error = "Item id does not match" });
+            }
+            await _repository.UpdateAsync(entity, id);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            bool isSuccesful = await _repository.DeleteAsync(id);
+            if (isSuccesful)
+                return NoContent();
+            return NotFound();
+        }
+    }
+}
---
+++ CollectorsAppBackend/CollectorsApp/Controllers/UserPreferencesController.cs
@@ -0,0 +1,53 @@
+﻿using CollectorsApp.Models;
+using CollectorsApp.Repository.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectorsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserPreferencesController : ControllerBase
+    {
+        private readonly IUserPreferencesRepository _repository;
+        public UserPreferencesController(IUserPreferencesRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserPreferences>>> GetAll()
+        {
+            return Ok(await _repository.GetAllAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserPreferences>> GetById(int id)
+        {
+            return Ok(await _repository.GetByIdAsync(id));
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post(UserPreferences entity)
+        {
+            await _repository.PostAsync(entity);
+            return CreatedAtAction("Created Succesfully", entity);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, UserPreferences entity)
+        {
+            if (id != entity.Id)
+            {
+                return BadRequest(new { error = "Item id does not match" });
+            }
+            await _repository.UpdateAsync(entity, id);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            bool isSuccesful = await _repository.DeleteAsync(id);
+            if (isSuccesful)
+                return NoContent();
+            return NotFound();
+        }
+
+    }
+}
