a197fac4f47731ed85dcc07c587d0c3fd8feffe0
Bartłomiej Kieroński <84330061+BartlomiejKieronski@users.noreply.github.com>
2025-09-18 10:44:43 +0200

    Refactor: UserService, add default preferences and consent entries on registerin  anew user, updated dto and model mapping to provide values only for needed fields

--- CollectorsAppBackend/CollectorsApp/Services/User/UserService.cs
+++ CollectorsAppBackend/CollectorsApp/Services/User/UserService.cs
@@ -1,7 +1,6 @@
 ﻿using CollectorsApp.Models;
 using CollectorsApp.Repository.Interfaces;
 using CollectorsApp.Services.Encryption;
-using ServiceStack;
 
 namespace CollectorsApp.Services.User
 {
@@ -10,36 +9,71 @@ namespace CollectorsApp.Services.User
         private readonly IDataHash _dataHash;
         private readonly IAesEncryption _aesEncryption;
         private readonly IUserRepository _userRepository;
-        public UserService(IDataHash dataHash, IAesEncryption aesEncryption, IUserRepository userRepository) 
+        private readonly IUserConsentRepository _userConsentRepository;
+        private readonly IUserPreferencesRepository _userPreferencesRepository;
+        public UserService(IDataHash dataHash, IAesEncryption aesEncryption, IUserRepository userRepository, IUserConsentRepository userConsentRepository, IUserPreferencesRepository userPreferences) 
         {
             _dataHash = dataHash;
             _aesEncryption = aesEncryption;
             _userRepository = userRepository;
+            _userConsentRepository = userConsentRepository;
+            _userPreferencesRepository = userPreferences;
         }
 
         public async Task<string> RegisterUserAsync(Users user)
         {
-            var credentials = await _dataHash.GetCredentialsAsync(user.Password);
-            var hashName = await _dataHash.GenerateHmacAsync(user.Name);
-            var hashEmail = await _dataHash.GenerateHmacAsync(user.Email);
-            var encryptedName = await _aesEncryption.AesEncrypt(user.Name);
-            var encryptedEmail = await _aesEncryption.AesEncrypt(user.Email);
+            try
+            {
+                var credentials = await _dataHash.GetCredentialsAsync(user.Password);
+                var hashName = await _dataHash.GenerateHmacAsync(user.Name);
+                var hashEmail = await _dataHash.GenerateHmacAsync(user.Email);
+                var encryptedName = await _aesEncryption.AesEncrypt(user.Name);
+                var encryptedEmail = await _aesEncryption.AesEncrypt(user.Email);
 
-            user.Email = encryptedEmail.Item1;
-            user.EmailIVKey = encryptedEmail.Item2;
-            user.HashedEmail = hashEmail;
-            user.Name = encryptedName.Item1;
-            user.NameIVKey = encryptedName.Item2;
-            user.HashedName = hashName;
-            user.Salt = credentials.Item1;
-            user.Password = credentials.Item2;
+                user.Email = encryptedEmail.Item1;
+                user.EmailIVKey = encryptedEmail.Item2;
+                user.HashedEmail = hashEmail;
+                user.Name = encryptedName.Item1;
+                user.NameIVKey = encryptedName.Item2;
+                user.HashedName = hashName;
+                user.Salt = credentials.Item1;
+                user.Password = credentials.Item2;
 
-            user.Active = true;
-            user.Role = "user";
-            user.AccountCreationDate = DateTime.Now;
-            user.IsBanned = false;
-            user.IsSusspended = false;
-            return await _userRepository.PostUser(user);
+                user.Active = true;
+                user.Role = "user";
+                user.AccountCreationDate = DateTime.Now;
+                user.IsBanned = false;
+                user.IsSusspended = false;
+
+                var succes = await _userRepository.PostUser(user);
+            
+                if (succes == "user exists")
+                {
+                    return "user exists";
+                }
+             
+                var IPConsent = new UserConsent() { OwnerId = Convert.ToInt32(succes), IsGranted=false,ConsentType = "IPAdress"};
+                var Id = new UserConsent() { OwnerId = Convert.ToInt32(succes), IsGranted = false, ConsentType = "PersonalIdentifyingData" };
+                var Id = new UserConsent() { OwnerId = Convert.ToInt32(succes), IsGranted = false, ConsentType = "DeviceID" };
+                var Analytics = new UserConsent() { OwnerId = Convert.ToInt32(succes), IsGranted = false, ConsentType = "Analytics" };
+                var ErrorReporting = new UserConsent() { OwnerId = Convert.ToInt32(succes), IsGranted = false, ConsentType = "ErrorReporting" };
+                var Cookies = new UserConsent() { OwnerId = Convert.ToInt32(succes), IsGranted = false, ConsentType = "Cookies" };
+            
+                await _userConsentRepository.PostAsync(IPConsent);
+                await _userConsentRepository.PostAsync(Id);
+                await _userConsentRepository.PostAsync(Analytics);
+                await _userConsentRepository.PostAsync(ErrorReporting);
+                await _userConsentRepository.PostAsync(Cookies);
+
+                var preferences = new UserPreferences() { OwnerId = Convert.ToInt32(succes), Theme = "dark", ItemsPerPage = 20, Layout = "Clasic", Pagination = true };
+
+                await _userPreferencesRepository.PostAsync(preferences);
+            }
+            catch
+            {
+                return "error creating user";
+            }
+            return "created succesfully";
         }
     }
 }
